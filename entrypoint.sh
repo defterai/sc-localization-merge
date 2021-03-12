ca4a782ee48883746ca7b4459e3ba013821cf9f7
Defter <defter@bigmir.net>
2021-03-12 02:24:36 +0200

    Fix last arguments pass

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 # Merge documents or exit if scripr fails
 set -eu
 
-printf "\nMerge documents %s to %s...\n" "${@:4}" "$1"
+printf "\nMerge documents %s to %s...\n" "$4" "$1"
 
-python3 /xlsx_to_ini_with_ref.py $1 $2 $3 "${@:4}" || { printf "\nUnable merge to %s file.\n" "$1"; exit 1; }
+python3 /xlsx_to_ini_with_ref.py $1 $2 $3 $4 || { printf "\nUnable merge to %s file.\n" "$1"; exit 1; }
 
 printf "\nSuccessfully merged to %s file.\n" "$1"
